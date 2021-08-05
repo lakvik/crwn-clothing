f78ff83b525a3e0c76e31d8b6c5590eb6eb0ebd8
Lakshitha Dayasena <lakshitha.dayasena@toptal.com>
2021-08-05 16:40:17 +0530

    Added addItem and removeItem functionality to checkout component.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,7 +5,7 @@ import {
   selectCartItems,
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -27,7 +27,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => (<CheckoutItem key={cartItem.id} cartItem={cartItem}></CheckoutItem>))}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem}></CheckoutItem>
+    ))}
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
